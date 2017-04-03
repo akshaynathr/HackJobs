dd6609994d09250ea046ca6091b6839a7ed0adcd
Akshaynath R <akshaynathr@gmail.com>
2017-04-03 23:51:04 +0530

    bug fix

--- app/views.py
+++ app/views.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template,request
+from flask import Flask, render_template,request,session
 from models import dbSetUp
 
 app=Flask(__name__)
+app.secret_key = 'F12Zr47j\3yX R~X@H!jmM]Lwf/,?KT'
 
 dbSetUp()
 
@@ -12,6 +13,37 @@ def home():
 
 
 
-@app.route('/add')
-def addContent():
-    return render_template('add.html')
+@app.route('/post',methods=['GET','POST'])
+def postJobs():
+    if request.method=='GET':
+        return render_template('add.html')
+    if request.method=='POST':
+        title=request.form['title']
+        link=request.form['link']
+        details=request.form['details']
+        if session['id']:
+
+            userid=session['id']
+            connection=r.connect('localhost',28015)
+            r.db('hackjobs').table('posts').insert({'userid':userid,'title':title,'time':r.now(),'link':link,'details':details }).run(connection)
+            connection.close()
+        else:
+            return redirect(url_for('login',))
+
+
+@app.route('/login',methods=['GET','POST'])
+def login():
+    if request.method=='GET':
+        if session['userid']:
+            pass
+        else:
+
+            return render_template('login.html')
+    if request.method=='POST':
+        username=request.form['username']
+        password=request.form['password']
+        connection=r.connect('localhost',28015)
+        user=list(r.db('hackjobs').table('user').filter((r.row['username']) & (r.row['password']) ).count().run(connection))
+        connection.close()
+
+        
