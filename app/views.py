5355e1ca5aabddce902d4315c3e966aa7530c58d
Akshaynath R <akshaynathr@gmail.com>
2017-04-15 17:19:15 +0530

    aaded new functions

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template,request,session
 from models import dbSetUp
+import rethinkdb as r
 
 app=Flask(__name__)
 app.secret_key = 'F12Zr47j\3yX R~X@H!jmM]Lwf/,?KT'
@@ -31,14 +32,20 @@ def postJobs():
             return redirect(url_for('login',))
 
 
+
+
+
 @app.route('/login',methods=['GET','POST'])
 def login():
     if request.method=='GET':
-        if session['userid']:
-            pass
-        else:
+        try:
+            if session['id']:
+                pass
+            else:
 
-            return render_template('login.html')
+                return render_template('login.html')
+        except :
+                return render_template('login.html')
     if request.method=='POST':
         username=request.form['username']
         password=request.form['password']
@@ -47,3 +54,37 @@ def login():
         connection.close()
 
         
+@app.route('/register',methods=['GET','POST'])
+def register():
+    if request.method=='GET':
+        try:
+            if session['id']:
+                pass
+            else:
+
+                return render_template('register.html')
+        except :
+                return render_template('register.html')
+    if request.method=='POST':
+        name=request.form['name']
+        email=request.form['email']
+        username=request.form['username']
+        password=request.form['password']
+        connection=r.connect('localhost',28015)
+        r.db('hackjobs').table('user').insert({'name':name,'username':username,'password':password,'email':email}).run(connection)
+        return "Done"
+
+@app.route('/add',methods=['GET','POST'])
+def add():
+    if request.method=='GET':
+        
+
+
+        return render_template('add.html')
+    else if request.method=='POST':
+        title=request.form['title']
+        link=request.form['link']
+        text=request.form['text']
+        connection=r.connect('localhost',28015)
+        r.db('hackjobs').table('post').insert({'title':title,'link':link,'text':text}).run(connection)
+        return "Done"
