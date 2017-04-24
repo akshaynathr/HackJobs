92e2b29c1cd3191c429525e189120487aa42953a
Akshaynath R <akshaynathr@gmail.com>
2017-04-24 23:09:06 +0530

    push integration

--- app/views.py
+++ app/views.py
@@ -220,4 +220,9 @@ def admin():
 #         return render_template('news.html',results=[],name=name,logout=logout,title="No news found")
 
 
+@app.route('/<path:path>')
+def static_proxy(path):
+  # send_static_file will guess the correct MIME type
+  return app.send_static_file(path)
+
 
